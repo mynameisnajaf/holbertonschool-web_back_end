9042d8c2b5834a66f8bb34cbbdb09b102034635e
abcdea <abcdea>
2025-12-19 17:42:29 +0300

    <-_->

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 9-insert school """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
