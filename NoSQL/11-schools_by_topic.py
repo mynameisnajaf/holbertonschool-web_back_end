24b90d3d856cd700362cee8ccb83baf0acb58fab
abcdea <abcdea>
2025-12-19 17:50:59 +0300

    <-_->

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
-    return list(mongo_collection.find({"topic":topic}))
+    return list(mongo_collection.find({"topics":topic}))
